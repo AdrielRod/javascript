c6664c1e48db5a838c9ce2a7af524895f233c39a
Adriel Rodrigues <117696260+AdrielRod@users.noreply.github.com>
2023-01-02 17:45:02 -0300

    aula 10 e sistema de multa

---
+++ Cursoemvideo/Aula11/ex001.js
@@ -0,0 +1,10 @@
+var vel = 500.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida`)
+} if (vel > 100) {
+    console.log(`Teu carro tá voando krl?`)
+} else{
+    console.log(`Você está nos conformes.`)
+}
---
+++ Cursoemvideo/Aula11/ex002.js
@@ -0,0 +1,8 @@
+console.clear
+
+var pais = "Eua"
+if (pais == "Brasil"){
+    console.log("Você é brasileira")
+} else{
+    console.log("Você é estrangeiro")
+}
