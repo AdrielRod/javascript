44ff8fa39c93d6da83d65e582779b3f08af3e140
Adriel Rodrigues <117696260+AdrielRod@users.noreply.github.com>
2023-01-07 21:44:05 -0300

    aula 14

---
+++ Cursoemvideo/Aula14/ambientejs.js
@@ -0,0 +1,9 @@
+var contador = 1
+
+console.log("Vai começar")
+for (var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log ("FIM")
+
+//A diferença entre ambas é que o for usa pouca linha e também aceita as 3 informações, como a váriavel que vai dizer se é verdadeiro ou falso, os limites e o contador.
