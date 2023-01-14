09078dc81296a3294f29cf784472154c5ae9b649
Adriel Rodrigues <117696260+AdrielRod@users.noreply.github.com>
2023-01-13 21:46:24 -0300

    anotação

--- anotações.js
+++ anotações.js
@@ -1,4 +1,6 @@
 /* Nesse dia, 11/01/2023, estava assitindo a aula da fábrica de aplicativo, no módulo de javascript básico e decidi testar o método join, por que não tinha ouvido falar.
 
+13.01/2023, eu já esqueci oq esse método faz.
+
 var lista = ['ola mundo', 'teste', 'testado']
 console.log(lista.join(')'))*/
